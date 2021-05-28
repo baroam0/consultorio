1a022201bcdeafc2457c2a1c819c47f986df09d7
Abel Barrios <barriosabel0@hotmail.com>
2021-05-27 23:45:56 -0300

    Se agrega estadistica

--- apps/estadisticas/views.py
+++ apps/estadisticas/views.py
@@ -1,12 +1,24 @@
 
 from django.shortcuts import render
 
+from apps.obrassociales.models import ObraSocial
+from apps.profesionales.models import Profesional
+
 
 def estadisticaasistencial(request):
     return render(request, 'estadisticas/estadistica_obrasocial.html')
 
 
 def estadisticaobrasocialmes(request):
-    return render(request, 'estadisticas/reporteobrasocialmes.html')
+    obrassociales = ObraSocial.objects.all().order_by("abreviatura")
+    profesionales = Profesional.objects.all()
+
+    return render(
+        request,
+        'estadisticas/reporteobrasocialmes.html',
+        {
+            'obrassociales': obrassociales,
+            'profesionales': profesionales
+        })
 
 # Create your views here.
